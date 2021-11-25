3acd1a9b08c860cbea4ec7f72415d2730d2ff89f
Mike Lee <1091085733@qq.com>
2021-11-25 11:11:21 +0800

    SortCompare

---
+++ src/Insertion.java
@@ -0,0 +1,76 @@
+
+/******************************************************************************
+ *  Compilation:  javac Insertion.java
+ *  Execution:    java Insertion < input.txt
+ *  Dependencies: StdOut.java StdIn.java
+ *  Data files:   https://algs4.cs.princeton.edu/21elementary/tiny.txt
+ *                https://algs4.cs.princeton.edu/21elementary/words3.txt
+ *  
+ *  Sorts a sequence of strings from standard input using insertion sort.
+ *
+ *  % more tiny.txt
+ *  S O R T E X A M P L E
+ *
+ *  % java Insertion < tiny.txt
+ *  A E E L M O P R S T X                 [ one string per line ]
+ *
+ *  % more words3.txt
+ *  bed bug dad yes zoo ... all bad yet
+ *
+ *  % java Insertion < words3.txt
+ *  all bad bed bug dad ... yes yet zoo   [ one string per line ]
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Insertion
+{
+    public static void sort(Comparable[] a)
+    {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j-1);
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) 
+    {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i ,int j)
+    {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a)
+    {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+    
+    public static boolean isSorted(Comparable[] a)
+    {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+    
+    public static void  main(String[] arg)
+    {
+        String[] a = In.readStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
