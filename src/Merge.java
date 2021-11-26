ad92c914cf63910d1d124871cf1469c475af1979
Mike Lee <1091085733@qq.com>
2021-11-26 11:30:36 +0800

    Merge

---
+++ src/Merge.java
@@ -0,0 +1,101 @@
+
+/******************************************************************************
+ *  Compilation:  javac Merge.java
+ *  Execution:    java Merge < input.txt
+ *  Dependencies: StdOut.java StdIn.java
+ *  Data files:   https://algs4.cs.princeton.edu/22mergesort/tiny.txt
+ *                https://algs4.cs.princeton.edu/22mergesort/words3.txt
+ *   
+ *  Sorts a sequence of strings from standard input using mergesort.
+ *   
+ *  % more tiny.txt
+ *  S O R T E X A M P L E
+ *
+ *  % java Merge < tiny.txt
+ *  A E E L M O P R S T X                 [ one string per line ]
+ *    
+ *  % more words3.txt
+ *  bed bug dad yes zoo ... all bad yet
+ *  
+ *  % java Merge < words3.txt
+ *  all bad bed bug dad ... yes yet zoo    [ one string per line ]
+ *  
+ ******************************************************************************/
+
+
+import edu.princeton.cs.algs4.*;
+
+public class Merge {
+
+    private Merge() {
+    }
+    
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        aux = new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+     }
+
+     
+    
+    private static boolean less(Comparable v, Comparable w) 
+    {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+
+        int i = lo, j = mid + 1;
+
+        // copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k]; 
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid)
+                a[k] = aux[j++];
+            else if (j > hi)
+                a[k] = aux[i++];
+            else if (less(aux[j], aux[i]))
+                a[k] = aux[j++];
+            else
+                a[k] = aux[i++];
+        }
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo)
+            return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+    
+    private static void show(Comparable[] a)
+    {//在单行中打印数组
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+    
+    public static boolean isSorted(Comparable[] a)
+    {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+    
+    public static void  main(String[] args)
+    {
+        String[] a = StdIn.readAllStrings();
+        Merge.sort(a);
+        // assert isSorted(a);
+        show(a);
+    }
+ }
